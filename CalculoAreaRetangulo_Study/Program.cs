8fa2abcb41aeb6d6e7e4c868c8424b4de74715d6
Paola <Paola@DESKTOP-L53JRFL>
2024-05-01 15:53:48 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoAreaRetangulo_Study/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double baseRetangulo, altura, areaRetangulo, perimetro, diagonal;
+
+        baseRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        areaRetangulo = baseRetangulo * altura;
+        perimetro = 2 * baseRetangulo + 2 * altura; 
+        //ou 2*(base+altura);
+
+        diagonal = Math.Sqrt(Math.Pow(baseRetangulo, 2.0) + Math.Pow(altura, 2.0)); 
+        //ou (base*base) + (altura*altura)
+
+        Console.WriteLine("Area = " + areaRetangulo.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+    
+        Console.ReadLine();
+    }
+
+}
+
